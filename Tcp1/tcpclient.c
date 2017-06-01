db843db399c2ebb174196db19daff76c71a6c77a
Debal Chakrabarty <debalchakrabarty@gmail.com>
2017-06-01 20:26:48 +0530

    Update tcpclient.c

--- Tcp1/tcpclient.c
+++ Tcp1/tcpclient.c
@@ -1,66 +1,46 @@
 #include<stdio.h>
-#include<netinet/in.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netdb.h>
 #include<string.h>
 #include<stdlib.h>
-#define MAX 80
-#define PORT 43454
-#define SA struct sockaddr
-void func(int sockfd)
+#include<strings.h>
+#include<sys/types.h> 
+#include<netinet/in.h> 
+#include<sys/socket.h>
+int main(int argc,char *argv[])
 {
-  char buff[MAX];
-  int n;
-  for(;;)
-  {
-    bzero(buff,sizeof(buff));
-    printf("Enter the string : ");
-    n=0;
-    while((buff[n++]=getchar())!='\n');
-    write(sockfd,buff,sizeof(buff));
-    bzero(buff,sizeof(buff));
-    read(sockfd,buff,sizeof(buff));
-    printf("From Server : %s",buff);
-    if((strncmp(buff,"exit",4))==0)
-    {
-      printf("Client Exit...\n");
-      break;
-    }
-   }
-  }
-
-  int main()
-  {
-    int sockfd,connfd;
-    struct sockaddr_in servaddr,cli;
-    
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-    
-    if(sockfd==-1)
-    {
-      printf("socket creation failed...\n");
-      exit(0);
-    }
-    else
-      printf("Socket successfully created..\n");
-    
-    bzero(&servaddr,sizeof(servaddr));
-    
-    servaddr.sin_family=AF_INET;
-    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    servaddr.sin_port=htons(PORT);
-    
-    if(connect(sockfd,(SA *)&servaddr,sizeof(servaddr))!=0)
-    {
-    printf("connection with the server failed...\n");
-    exit(0);
-    }
-    else
-    printf("connected to the server..\n");
-    
-    func(sockfd);
-    
-    close(sockfd);
-  }
-
+	int sockfd,newsockfd,i,j,k,l;
+	char msg[100];
+	struct sockaddr_in serv_addr;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd<0)
+	{
+		printf("socket creation failed\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created\n");
+	bzero(&serv_addr,sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
+	{
+		printf("error on connecting\n");
+		exit(0);
+	}
+	else
+		printf("connected to the server\n");
+	while(1)
+	{
+		memset(msg,0,100);
+		printf("Enter the string to be sent to the: \n");
+		fgets(msg,100,stdin);
+		write(sockfd,msg,100);
+		memset(msg,0,100);
+		read(sockfd,msg,100);
+		printf("the message from server : %s \n");
+	}
+	close(sockfd);
+}
+		
+	
+	
