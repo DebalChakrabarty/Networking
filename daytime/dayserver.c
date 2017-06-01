bb54353ee74ec9eb32f470a9004d949b592475b0
Debal Chakrabarty <debalchakrabarty@gmail.com>
2017-06-01 20:29:13 +0530

    Update dayserver.c

--- daytime/dayserver.c
+++ daytime/dayserver.c
@@ -1,37 +1,49 @@
-#include<time.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
 #include<stdio.h>
 #include<string.h>
+#include<strings.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<time.h>
+#include<sys/socket.h>
 #include<netinet/in.h>
-#include<netdb.h>
 int main(int argc,char *argv[])
 {
-	int sd,ad;
-	char buff[1024];
-	struct sockaddr_in servaddr,cliaddr;
-	
-	time_t t1;
-	bzero(&servaddr,sizeof(servaddr));
-	
-	servaddr.sin_family=AF_INET;
-	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-	servaddr.sin_port=htons(1507);
-	sd=socket(AF_INET,SOCK_STREAM,0);
-	
-	bind(sd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-	listen(sd,5);
-	
-	printf("Server is running...\n");
-	ad=accept(sd,(struct sockaddr *)NULL,NULL);
-	
-	while(1)
+	int sockfd,newsockfd,clilen,i,j,k,l,port;
+	struct sockaddr_in serv_addr,cli_addr;
+	char times[100];
+	time_t time1;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd<0)
 	{
-		bzero(&buff,sizeof(buff));
-		t1=time(NULL);
-		snprintf(buff,sizeof(buff),"%24s\r\n",ctime(&t1));
-		send(ad,buff,sizeof(buff),0);
+		printf("Error on socket creation");
+		exit(0);
 	}
-	
+	bzero((char *)&serv_addr,sizeof(serv_addr));
+	port=atoi(argv[1]);
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=INADDR_ANY;
+	serv_addr.sin_port=htons(port);
+	if(bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
+	{
+		printf("error on binding\n");
+		exit(0);
+	}
+	clilen=sizeof(cli_addr);
+	listen(sockfd,5);
+	newsockfd=accept(sockfd,(struct sockaddr *)&cli_addr,&clilen);
+	if(newsockfd<0)
+	{
+		printf("error on accept\n");
+	}
+	else
+	{
+		memset(times,0,100);
+		time1=time(NULL);
+		sprintf(times,"%s",ctime(&time1));
+		write(newsockfd,times,100);
+	}
+	close(newsockfd);
+	close(sockfd);
 }
+		
+	
